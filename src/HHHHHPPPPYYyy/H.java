42e48a634258092c110de4bb00d77ff1b60d10bb
League Student <league@iMac-22.attlocal.net>
2019-11-17 12:44:51 -0800

    sfegtrumk,m.mmgtmkjfkyrtjklrt5 µeui4 ≤≥∆7z8≥ ∞†˙∫∫˙ cx≈≈tu. 7 ∞98y6 

---
+++ src/HHHHHPPPPYYyy/H.java
@@ -0,0 +1,48 @@
+package HHHHHPPPPYYyy;
+
+import javax.swing.JOptionPane;
+
+public class H {
+public static void main(String[] args) {
+	
+String xhmxdhrhiouu88iuu8iy98909lol9l=	JOptionPane.showInputDialog("are you happy" );
+		
+	if(xhmxdhrhiouu88iuu8iy98909lol9l.equals( "yes")){
+		JOptionPane.showMessageDialog(null, "I AM MAKING MAC AND CHEESE!!!");
+	}
+	else if(xhmxdhrhiouu88iuu8iy98909lol9l.equals("no")) {
+	String t=JOptionPane.showInputDialog("do you want to be happy");
+		if(t.equals("yes")) {JOptionPane.showMessageDialog(null, "die in a fire");}
+		else if(t.equals("no")) {JOptionPane.showMessageDialog(null, "that is amazing");}
+	
+	
+	
+	}
+
+	}
+	
+			
+			
+			
+			
+			
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}

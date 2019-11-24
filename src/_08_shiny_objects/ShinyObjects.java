63398ed7a744c2572675dca6c6ac7191532d82c8
League Student <league@iMac-22.attlocal.net>
2019-11-24 12:58:09 -0800

    kl;jkfrhjhrtgurtbu9jny5 5rt  v mg tgfv mgv gjhmn bjhmm fgvmgbff

--- src/_08_shiny_objects/ShinyObjects.java
+++ src/_08_shiny_objects/ShinyObjects.java
@@ -8,16 +8,21 @@ import java.applet.AudioClip;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 		// 2. Ask the user how many shiny objects they want
-
+String s=JOptionPane.showInputDialog("HOW MEANY MAC AND CHEESE DO YOU WANT");
 		// 3. Play the sound that many times
-
+int y= Integer.parseInt(s);
+int x = 0;
+while(x<y) {
 		// 1. Call the method below
-
+playMisterZee();
+x++;
+}
 	}
 
 	public static void playMisterZee() {
--- src/_09_robot_walk_of_fame/WalkOfFame.java
+++ src/_09_robot_walk_of_fame/WalkOfFame.java
@@ -4,6 +4,8 @@
 
 package _09_robot_walk_of_fame;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
@@ -11,10 +13,44 @@ public class WalkOfFame {
 		Robot rob = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
+rob.setX(250);
+rob.setY(75);
+rob.setRandomPenColor();
+rob.penDown();
+rob.setSpeed(99999);
+rob.setWindowColor(Color.white);
+// 2. Make the robot draw a star shape. Hint: 144.
 
-		// 2. Make the robot draw a star shape. Hint: 144.
 
-		// 3. Set the size of the star to 30.
+for (int i = 0; i < 5; i++) {
+	rob.turn(144);rob.move(200);
+	rob.setRandomPenColor();
+	rob.turn(144);rob.move(200);
+	rob.setRandomPenColor();
+	rob.turn(144);rob.move(200);
+	rob.setRandomPenColor();
+	rob.turn(144);rob.move(200);
+	rob.setRandomPenColor();
+	rob.turn(144);rob.move(200);
+	rob.turn(144);rob.move(200);
+	rob.setRandomPenColor();
+	rob.turn(144);rob.move(200);
+	rob.setRandomPenColor();
+	rob.turn(144);rob.move(400);
+}
+for (int i = 0; i < 5; i++) {
+rob.setPenColor(Color.white);
+rob.setPenWidth(999);
+rob.turn(144);rob.move(200);
+rob.turn(144);rob.move(200);
+rob.turn(144);rob.move(200);
+rob.turn(144);rob.move(200);
+rob.turn(144);rob.move(200);
+rob.turn(144);rob.move(200);
+rob.turn(144);rob.move(200);
+rob.turn(144);rob.move(400);
+}
+// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
